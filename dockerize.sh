9487578dff60c5e2ed7e3c023d04c085b9eb19a4
Huseyin ERDEM <rootofarch@gmail.com>
2019-06-23 19:27:00 +0300

    made the script useful with a lot of parameters(still has defaults), made help menus, changed dockers work dir to home dir and set Tmux instead of bash while running

--- dockerize.sh
+++ dockerize.sh
@@ -8,17 +8,10 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-
-if [ -z "$ROS_VERSION" ] ; then
-  ROS_VERSION=kinetic
-fi
-
 DOCKER_PATH=$DIR
-CONTAINER_NAME=${USER}_${ROS_VERSION}
-CONTAINER_IMAGE=${USER}:${ROS_VERSION}
 
 function build {
-    docker build --build-arg ROS_VERSION=${ROS_VERSION} -t $CONTAINER_IMAGE $DOCKER_PATH
+    docker build --build-arg ROS_VERSION=${ROS_VERSION} -t ${USER}:${ROS_VERSION} $DOCKER_PATH
 }
 
 function run {
@@ -28,21 +21,31 @@ function run {
     # PROJECT SPECIFIC
     docker run \
         -it \
-        --rm \
         --privileged \
+        --rm \
         --user $(id -u):$(id -g) \
- 		  	--env="DISPLAY=${DISPLAY}" \
-        --env="QT_X11_NO_MITSHM=1" \
-    		--volume="/etc/group:/etc/group:ro" \
-    		--volume="/etc/passwd:/etc/passwd:ro" \
-    		--volume="/etc/shadow:/etc/shadow:ro" \
-    		--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-    		--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+            --env="DISPLAY=${DISPLAY}" \
+            --env="QT_X11_NO_MITSHM=1" \
+            --volume="/etc/group:/etc/group:ro" \
+            --volume="/etc/passwd:/etc/passwd:ro" \
+            --volume="/etc/shadow:/etc/shadow:ro" \
+            --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+            --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --workdir $HOME \
         --device /dev/dri/ \
         --device /dev/vga_arbiter/ \
+        --device /dev/input/ \
         --volume $HOME:$HOME \
         --name $CONTAINER_NAME \
-        $CONTAINER_IMAGE /bin/bash
+        $CONTAINER_IMAGE tmux
+}
+
+function attach {
+    docker exec -it $CONTAINER_NAME $COMMAND
+}
+
+function stop {
+    docker stop $CONTAINER_NAME &> /dev/null
 }
 
 function clean {
@@ -57,22 +60,146 @@ function clean {
     fi
 }
 
-function attach {
-    docker exec -it $CONTAINER_NAME /bin/bash
-}
 
-function stop {
-    docker stop $CONTAINER_NAME &> /dev/null
-}
 
 case "$1" in
-    build|run|attach|stop|clean)
-        $1
-        ;;
-    *)
-        echo $"Usage: $0 {clean|build|run|attach|stop}"
-        echo $"   Consumed env varilabes:"
-        echo $"     - ROS_VERSION        "
+    build ) shift
+        while [ "$1" != "" ]; do
+                case $1 in
+                    -v | --ros_version )    shift
+                                            ROS_VERSION=$1
+                                            ;;
+                    * )                     echo "Builds container image which has ros and some useful tools"
+                                            echo "Usage:"
+                                            echo "-v | --ros_version    : sets ros distro which created image will include. Default: kinetic"
+                                            echo "-h | --help           : displays this message"
+                                            exit 1
+                                            ;;
+                esac
+                shift
+            done
+            if [ -z "$ROS_VERSION" ] ; then
+                ROS_VERSION=kinetic
+            fi
+            echo "Ros Version Set To ${ROS_VERSION}"
+        build
+    ;;
+    run ) shift
+        while [ "$1" != "" ]; do
+            case $1 in
+                -n | --container_name ) shift
+                                        CONTAINER_NAME=$1
+                                        ;;
+                -v | --ros_version )    shift
+                                        ROS_VERSION=$1
+                                        CONTAINER_NAME=${USER}_${ROS_VERSION}
+                                        ;;
+                -p | --project )        shift
+                                        CONTAINER_IMAGE=${USER}:${1}
+                                        CONTAINER_NAME=${USER}_${1}
+                                        ;;
+                -i | --image )          shift
+                                        CONTAINER_IMAGE=$1
+                                        ;;
+                * )                     echo "Creates and attaches a container with some options"
+                                        echo "Usage:"
+                                        echo "-n | --container_name : sets the container name which will be created. Default: \${USER}_\${CONTAINER_IMAGE}"
+                                        echo "-v | --ros_version    : sets ros distro which defines container image as \$USER:nvidia-\$ROS_VERSION"
+                                        echo "-p | --project        : sets project name which defines container image as \$USER:project. With this option the ros_version parameter(-v) will be overwriten"
+                                        echo "-i | --image          : sets the docker image. With this option the other parameters except container name(-n) will be overwriten"
+                                        echo "-h | --help           : displays this message"
+                                        exit 1
+                                        ;;
+            esac
+            shift
+        done
+        if [ -z "$ROS_VERSION" ] ; then
+            ROS_VERSION=kinetic
+        fi
+        if [ -z "$CONTAINER_IMAGE" ] ; then
+            echo "Ros Version Set To ${ROS_VERSION}"
+            CONTAINER_IMAGE=${USER}:nvidia-${ROS_VERSION}
+        fi
+        if [ -z "$CONTAINER_NAME" ] ; then
+            CONTAINER_NAME=${USER}_${ROS_VERSION}
+        fi
+        echo "Container Name Set To ${CONTAINER_NAME}"
+        echo "CONTAINER_Image Set To ${CONTAINER_IMAGE}"
+        run
+    ;;
+    attach ) shift
+        while [ "$1" != "" ]; do
+                case $1 in
+                    -n | --container_name ) shift
+                                            CONTAINER_NAME=$1
+                                            ;;
+                    -c | --command ) shift
+                                            COMMAND=$1
+                                            ;;
+                    * )                     echo "Attaches a container with a command"
+                                            echo "Usage:"
+                                            echo "-n | --container_name : sets the container name which will be attached. Default: \${USER}_kinetic"
+                                            echo "-c | --command        : sets the command which will run Default: /bin/bash"
+                                            echo "-h | --help           : displays this message"
+                                            exit 1
+                                            ;;
+                esac
+                shift
+            done
+            if [ -z "$CONTAINER_NAME" ] ; then
+                CONTAINER_NAME=${USER}_kinetic
+            fi
+            if [ -z "$COMMAND" ] ; then
+                COMMAND=/bin/bash
+            fi
+            echo "Container Name Set To ${CONTAINER_NAME}"
+            echo "Command Set To ${COMMAND}"
+        attach
+    ;;
+    stop ) shift
+        while [ "$1" != "" ]; do
+                case $1 in
+                    -n | --container_name ) shift
+                                            CONTAINER_NAME=$1
+                                            ;;
+                    * )                     echo "Stops a running container"
+                                            echo "Usage:"
+                                            echo "-n | --container_name : sets the container name which will be attached. Default: \${USER}_kinetic"
+                                            echo "-h | --help           : displays this message"
+                                            exit 1
+                                            ;;
+                esac
+                shift
+            done
+            if [ -z "$CONTAINER_NAME" ] ; then
+                CONTAINER_NAME=${USER}_kinetic
+            fi
+            echo "Container Name Set To ${CONTAINER_NAME}"
+        stop
+    ;;
+    clean ) shift
+        if [ "$1" != "" ]; then
+            echo "Removes all the containers and images"
+            echo "Usage:"
+            echo "-h | --help           : displays this message"
+            exit 1
+        fi
+        echo "Please type Yes to verify cleaning"
+        read Verification
+        if [ "$Verification" == "Yes" ]; then
+            clean
+        else
+            echo "Cleaning could not be verified"
+        fi
+    ;;
+    *)  echo "Dockerize has following functions. Please type --help with any command for features. Example:  dockerize run --help"
+        echo
+        echo
+        echo "build     :Builds a container image which has ros and some useful tools"
+        echo "run       :Creates and attaches a container with some options"
+        echo "attach    :Attaches a container with a command"
+        echo "stop      :Stops a running container"
+        echo "clean     :Removes all the containers and images"
         exit 1
         ;;
 esac
